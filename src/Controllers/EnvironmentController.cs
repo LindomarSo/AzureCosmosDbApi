4a69fd634f5463b185e6d68ff51a5db7ed68a820
Lindomar de Souza Dias <lindomardias21@gmail.com>
2023-07-30 18:06:15 -0300

    feat: controller de environments

---
+++ src/Controllers/EnvironmentController.cs
@@ -0,0 +1,22 @@
+﻿using AzureCosmosDbClientApi.Models;
+using AzureCosmosDbClientApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace AzureCosmosDbClientApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EnvironmentController : ControllerBase
+{
+    private readonly IConfiguration configuration;
+
+    public EnvironmentController(IConfiguration configuration)
+    {
+        this.configuration = configuration;
+    }
+
+    [HttpGet()]
+    public ActionResult GetContainerProperties([FromRoute] ContainerViewModel container) 
+        => Ok(this.configuration["Environment"]);
+}
